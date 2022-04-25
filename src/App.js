ee73014fdf1d82c424b29e1aac2eeebb18a0ee23
quarter_lab <quarterlab93@gmail.com>
2022-04-26 05:51:58 +0900

    データ取得部分の非同期処理を修正

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ function App() {
   const [value, setValue] = useState('---');
   const [apiValue, setApiValue] = useState('***');
 
-  useEffect(async ()=>{
-    const response = await fetch('/.auth/me');
-    const payload = await response.json();
-    const { clientPrincipal } = payload;
-
-    if(clientPrincipal){
-      setValue(clientPrincipal.userDetails);
+  useEffect(()=>{
+    async function fetchData() {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+  
+      if(clientPrincipal){
+        setValue(clientPrincipal.userDetails);
+      }
+  
+      const apires = await fetch('/api/HttpTrigger2');
+      const apiPayload = await apires.json();
+      const { apiClientPrincipal } = apiPayload;
+  
+      if(apiClientPrincipal){
+        setApiValue(apiClientPrincipal.userDetails);
+      }
     }
-
-    const apires = await fetch('/api/HttpTrigger2');
-    const apiPayload = await apires.json();
-    const { apiClientPrincipal } = apiPayload;
-
-    if(apiClientPrincipal){
-      setApiValue(apiClientPrincipal.userDetails);
-    }
+    fetchData();
   },[]);
 
 
